e5f84e67f900adf8fe989b0eb674caf83fa001c8
tashi tsering <tashi.cse@gmail.com>
2019-12-15 22:15:30 +0100

    added doc strings and comments for classes.py file

--- classes.py
+++ classes.py
@@ -7,52 +7,85 @@ import os
 
 
 class Maze:
-    """This is a class for  creating a maze"""
+    """This is a class for creating a maze"""
 
     def __init__(self, filename):
+        """
+        The constructor for Maze class.
+
+        Parameters:
+        filename : maze file in a text file format.
+        """
         self.filename = filename
         self.structure = 0
 
     def maze_generator(self):
+        """ Method to read the maze file and creates a list with contains a list per line"""
 
         # Opening of file
         try:
             with open(self.filename, "r") as file_maze:
                 structure_level = []
+                # loop through the lines in the file
                 for line in file_maze:
                     line_level = []
+                    # loop through the sprites or characters in the file
                     for sprite in line:
+                        # ignore the "\ n" at the end of the line
                         if sprite != '\n':
+                            # append the sprite to the line_level list
                             line_level.append(sprite)
+                    # append the lines to the list structure_level
                     structure_level.append(line_level)
+                    # assign or save to structure
                 self.structure = structure_level
         except IOError:
             print("Not able to open the file {}.txt".format(self.filename))
             os.exit(1)
 
     def display(self, window):
+        """
+        Method for displaying the maze  according the the list resturn by the previous function
+
+
+        Parameters:
+           window: This function will create a display Surface. The arguments passed in are requests for a display type
+        """
         num_line = 0
         for line in self.structure:
             num_case = 0
+            # loop through the lines
             for sprite in line:
+                # calculate the real position in pixel
                 x = num_case * MATRIX_SIZE
                 y = num_line * MATRIX_SIZE
-                if sprite == 'm':  # m = wall
+                if sprite == 'm':  # m = Wall
                     window.blit(WALL, (x, y))
-                elif sprite == 'd':  # d = start
+                elif sprite == 'd':  # d = Start
                     window.blit(DEPART, (x, y))
-                elif sprite == 'F':  # F = destination
+                elif sprite == 'F':  # F = Destination
                     window.blit(DESTINATION, (x, y))
                 elif sprite == '0':
-                    window.blit(FLOOR, (x, y))
+                    window.blit(FLOOR, (x, y))  # 0 = Floor
                 num_case += 1
             num_line += 1
 
 
 class Player:
-    """ This is a class for creating player in the game """
+    """
+        This is a class for creating character in the game.
+
+
+     """
 
     def __init__(self, level):
+        """
+        The constructor for Player class.
+
+        Parameters:
+        level : list
+        """
+        # player position in boxes and pixels
         self.case_x = 0
         self.case_y = 0
         self.x = 0
@@ -60,6 +93,14 @@ class Player:
         self.level = level
 
     def control(self, direction):
+        """
+                The method is use to control the movement of the player character .
+
+                Parameters:
+                    direction: constants (from pygame.localspygame constants) used to represent keyboard keys.
+
+
+         """
 
         # right movement
         if direction == 'right':
@@ -91,7 +132,19 @@ class Player:
 
 
 class Items:
+    """
+        This is a class for creating items in the game.
+
+
+    """
     def __init__(self, structure):
+        """
+        The constructor for Items  class.
+
+        Parameters:
+                  structure : list.
+         """
+        # Initialising  item  position in boxes and pixels
         self.structure = structure
         self.case_x = 0
         self.case_y = 0
@@ -99,6 +152,11 @@ class Items:
         self.y = 0
 
     def items_position(self):
+        """
+        The method to set item positions on the maze.
+
+
+        """
         while self.structure[self.case_y][self.case_x] != "0":
             self.case_x = random.randint(0, 14)
             self.case_y = random.randint(0, 14)
